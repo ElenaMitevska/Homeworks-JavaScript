9f8f8a29107625f4793a89729e3ddad4ce165a49
unknown <elena_mitevska5@yahoo.com>
2023-12-05 20:15:10 +0100

    e

---
+++ Homework 01/homework01.js
@@ -0,0 +1,44 @@
+
+/*1. Write a JavaScript program that calculates the total price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%. Print the result in the console.*/
+let price = 119.95;
+let Phones = 30;
+const taxRate = 1.05;
+
+let result = 30 * price * taxRate;
+console.log('The result is: $', result);
+
+/*2. Write a JavaScript program that takes minutes and converts them to seconds. Print the result in the console.*/
+const conversionValue = 60;
+let min = 5;
+let seconds = min * conversionValue;
+console.log(seconds);
+
+/*3. Write a JavaScript program that takes a number and increments the number by +1 and prints the result in the console.*/
+let number = 1;
+let incrementedNum = ++number;
+console.log('The result is', incrementedNum);
+
+/*4. Write a JavaScript program that takes the base and height of a triangle and calculates its area. Print the result in the console.*/
+
+
+let sideA = 5;
+let height = 7;
+let triangleArea = 0.5 * sideA * height
+console.log('The triangleArea is (in centimeters):', triangleArea);
+
+/*5. Write a JavaScript program that takes length and width of a rectangle and finds its perimeter. Print the result in the console.*/
+
+
+let length = 10;
+let width = 20;
+let perimeter = (length + width) * 2;
+console.log('The perimeter of rectangle is:', perimeter)
+
+/*6. Write a JavaScript program that will calculate area of circle. Print the result in the console*/
+const Pi = 3.14159;
+let radius = 6;
+let circleArea = Pi * (radius ** 2);
+console.log('The circle Area is:', circleArea);
+
+
+
