651768bf293607005c7350aa9bd64272fb549703
unknown <elena_mitevska5@yahoo.com>
2023-12-17 21:03:48 +0100

    l

---
+++ Homework 05/exercise2.js
@@ -0,0 +1,21 @@
+2. /*HOMEWORK PART 2
+Print all numbers from an array and the sum
+
+Create an array with numbers
+Print all numbers from the array in a list element, in a new HTML page
+Print out the sum of all of the numbers below the list
+Bonus: Try printing the whole mathematical equasion as well ( 2 + 4 + 5 = 11)*/
+
+
+array = [1, 2, 3, 4, 5]
+
+
+for (let i = 1; i <= 5; i++) {
+    let container = document.querySelector("#container");
+    let li = document.createElement("li")
+    li.innerText = `${i}`;
+    container.appendChild(li);
+}
+
+let result = document.querySelector("#result")
+result.innerText = `The sum of all number is: ${1 + 2 + 3 + 4 + 5}`;
---
+++ Homework 05/homework05.js
@@ -0,0 +1,21 @@
+1. /*HOMEWORK PART 1
+Change the page with JavaScript
+
+// Change the text of all paragraphs and headers with javascript*/
+
+let header = document.querySelectorAll("h1");
+header[0].innerText = "Header text is changed";
+header[1].innerText = "Header text is changed";
+
+let headerThree = document.querySelector("h3");
+headerThree.innerText = "And this header text is changed"
+
+
+let paragraph = document.querySelectorAll(".paragraph");
+paragraph[0].innerText = "The paragraph text is changed";
+paragraph[1].innerText = "The paragraph text is changed";
+
+
+
+
+
